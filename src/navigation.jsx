afe0ec89f7d7b6cb88ad2a984c27981969a945db
chun37 <chun.jb.37@gmail.com>
2021-03-08 06:00:17 +0900

    設定画面に切り替え出来る

--- src/navigation.jsx
+++ src/navigation.jsx
@@ -4,14 +4,11 @@ import { Link } from "react-router-dom";
 const Navigation = () => (
   <nav>
     <ul>
-      <li>
+      <li key="home">
         <Link to="/">Home</Link>
       </li>
       <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/users">Users</Link>
+        <Link to="/settings">Settings</Link>
       </li>
     </ul>
   </nav>
--- src/route.jsx
+++ src/route.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Settings from "./settings";
 
 const Routing = () => (
   <Switch>
-    <Route path="/about" exact>
-      <About />
-    </Route>
-    <Route path="/users" exact>
-      <Users />
-    </Route>
     <Route path="/" exact>
       <Home />
+    </Route>
+    <Route path="/settings">
+      <Settings />
     </Route>
   </Switch>
 );
 
 const Home = () => <h2>Home</h2>;
-
-const About = () => <h2>About</h2>;
-
-const Users = () => <h2>Users</h2>;
 
 export default Routing;
