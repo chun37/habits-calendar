c5ab7cbbcf386d50f03065fc4090d90fb22a8dfe
chun37 <chun.jb.37@gmail.com>
2021-03-14 06:25:11 +0900

    onSubmitをやめた

--- src/components/settings/TaskForm.jsx
+++ src/components/settings/TaskForm.jsx
@@ -4,26 +4,25 @@ import db from "../../db";
 const AddTask = () => {
   const [task, setTask] = useState();
 
-  const onSubmit = (event) => {
+  const addTask = () => {
     if (!task) return;
     db.tasks.add({ name: task });
-    event.preventDefault();
     setTask("");
   };
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="task">
-          タスク名:
-          <input
-            type="text"
-            value={task}
-            onChange={(event) => setTask(event.target.value)}
-          />
-        </label>
-        <button type="submit">追加</button>
-      </form>
+      <label>
+        タスク名:
+        <input
+          type="text"
+          value={task}
+          onChange={(event) => setTask(event.target.value)}
+        />
+      </label>
+      <button type="submit" onClick={addTask}>
+        追加
+      </button>
     </div>
   );
 };
